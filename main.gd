0bb43f74e2ccb7a3d6f7416674e0cb32599f7674
binury <1146921+binury@users.noreply.github.com>
2025-08-24 15:09:07 -0500

    Refactor: config file loading

--- main.gd
+++ main.gd
@@ -1,5 +1,401 @@
 extends Node
 
+enum MidiNote {
+	C_neg1,
+	Cs_neg1,
+	D_neg1,
+	Ds_neg1,
+	E_neg1,
+	F_neg1,
+	Fs_neg1,
+	G_neg1,
+	Gs_neg1,
+	A_neg1,
+	As_neg1,
+	B_neg1,
+	C0,
+	Cs0,
+	D0,
+	Ds0,
+	E0,
+	F0,
+	Fs0,
+	G0,
+	Gs0,
+	A0,
+	As0,
+	B0,
+	C1,
+	Cs1,
+	D1,
+	Ds1,
+	E1,
+	F1,
+	Fs1,
+	G1,
+	Gs1,
+	A1,
+	As1,
+	B1,
+	C2,
+	Cs2,
+	D2,
+	Ds2,
+	E2,
+	F2,
+	Fs2,
+	G2,
+	Gs2,
+	A2,
+	As2,
+	B2,
+	C3,
+	Cs3,
+	D3,
+	Ds3,
+	E3,
+	F3,
+	Fs3,
+	G3,
+	Gs3,
+	A3,
+	As3,
+	B3,
+	C4,
+	Cs4,
+	D4,
+	Ds4,
+	E4,
+	F4,
+	Fs4,
+	G4,
+	Gs4,
+	A4,
+	As4,
+	B4,
+	C5,
+	Cs5,
+	D5,
+	Ds5,
+	E5,
+	F5,
+	Fs5,
+	G5,
+	Gs5,
+	A5,
+	As5,
+	B5,
+	C6,
+	Cs6,
+	D6,
+	Ds6,
+	E6,
+	F6,
+	Fs6,
+	G6,
+	Gs6,
+	A6,
+	As6,
+	B6,
+	C7,
+	Cs7,
+	D7,
+	Ds7,
+	E7,
+	F7,
+	Fs7,
+	G7,
+	Gs7,
+	A7,
+	As7,
+	B7,
+	C8,
+	Cs8,
+	D8,
+	Ds8,
+	E8,
+	F8,
+	Fs8,
+	G8,
+	Gs8,
+	A8,
+	As8,
+	B8,
+	C9,
+	Cs9,
+	D9,
+	Ds9,
+	E9,
+	F9,
+	Fs9,
+	G9
+}
+
+const MIDI_NOTE_NAMES := {
+	0: "C-1",
+	1: "C#-1",
+	2: "D-1",
+	3: "D#-1",
+	4: "E-1",
+	5: "F-1",
+	6: "F#-1",
+	7: "G-1",
+	8: "G#-1",
+	9: "A-1",
+	10: "A#-1",
+	11: "B-1",
+	12: "C0",
+	13: "C#0",
+	14: "D0",
+	15: "D#0",
+	16: "E0",
+	17: "F0",
+	18: "F#0",
+	19: "G0",
+	20: "G#0",
+	21: "A0",
+	22: "A#0",
+	23: "B0",
+	24: "C1",
+	25: "C#1",
+	26: "D1",
+	27: "D#1",
+	28: "E1",
+	29: "F1",
+	30: "F#1",
+	31: "G1",
+	32: "G#1",
+	33: "A1",
+	34: "A#1",
+	35: "B1",
+	36: "C2",
+	37: "C#2",
+	38: "D2",
+	39: "D#2",
+	40: "E2",
+	41: "F2",
+	42: "F#2",
+	43: "G2",
+	44: "G#2",
+	45: "A2",
+	46: "A#2",
+	47: "B2",
+	48: "C3",
+	49: "C#3",
+	50: "D3",
+	51: "D#3",
+	52: "E3",
+	53: "F3",
+	54: "F#3",
+	55: "G3",
+	56: "G#3",
+	57: "A3",
+	58: "A#3",
+	59: "B3",
+	60: "C4",
+	61: "C#4",
+	62: "D4",
+	63: "D#4",
+	64: "E4",
+	65: "F4",
+	66: "F#4",
+	67: "G4",
+	68: "G#4",
+	69: "A4",
+	70: "A#4",
+	71: "B4",
+	72: "C5",
+	73: "C#5",
+	74: "D5",
+	75: "D#5",
+	76: "E5",
+	77: "F5",
+	78: "F#5",
+	79: "G5",
+	80: "G#5",
+	81: "A5",
+	82: "A#5",
+	83: "B5",
+	84: "C6",
+	85: "C#6",
+	86: "D6",
+	87: "D#6",
+	88: "E6",
+	89: "F6",
+	90: "F#6",
+	91: "G6",
+	92: "G#6",
+	93: "A6",
+	94: "A#6",
+	95: "B6",
+	96: "C7",
+	97: "C#7",
+	98: "D7",
+	99: "D#7",
+	100: "E7",
+	101: "F7",
+	102: "F#7",
+	103: "G7",
+	104: "G#7",
+	105: "A7",
+	106: "A#7",
+	107: "B7",
+	108: "C8",
+	109: "C#8",
+	110: "D8",
+	111: "D#8",
+	112: "E8",
+	113: "F8",
+	114: "F#8",
+	115: "G8",
+	116: "G#8",
+	117: "A8",
+	118: "A#8",
+	119: "B8",
+	120: "C9",
+	121: "C#9",
+	122: "D9",
+	123: "D#9",
+	124: "E9",
+	125: "F9",
+	126: "F#9",
+	127: "G9"
+}
+
+const NOTE_TO_MIDI := {
+	"C-1": 0,
+	"C#-1": 1,
+	"D-1": 2,
+	"D#-1": 3,
+	"E-1": 4,
+	"F-1": 5,
+	"F#-1": 6,
+	"G-1": 7,
+	"G#-1": 8,
+	"A-1": 9,
+	"A#-1": 10,
+	"B-1": 11,
+	"C0": 12,
+	"C#0": 13,
+	"D0": 14,
+	"D#0": 15,
+	"E0": 16,
+	"F0": 17,
+	"F#0": 18,
+	"G0": 19,
+	"G#0": 20,
+	"A0": 21,
+	"A#0": 22,
+	"B0": 23,
+	"C1": 24,
+	"C#1": 25,
+	"D1": 26,
+	"D#1": 27,
+	"E1": 28,
+	"F1": 29,
+	"F#1": 30,
+	"G1": 31,
+	"G#1": 32,
+	"A1": 33,
+	"A#1": 34,
+	"B1": 35,
+	"C2": 36,
+	"C#2": 37,
+	"D2": 38,
+	"D#2": 39,
+	"E2": 40,
+	"F2": 41,
+	"F#2": 42,
+	"G2": 43,
+	"G#2": 44,
+	"A2": 45,
+	"A#2": 46,
+	"B2": 47,
+	"C3": 48,
+	"C#3": 49,
+	"D3": 50,
+	"D#3": 51,
+	"E3": 52,
+	"F3": 53,
+	"F#3": 54,
+	"G3": 55,
+	"G#3": 56,
+	"A3": 57,
+	"A#3": 58,
+	"B3": 59,
+	"C4": 60,
+	"C#4": 61,
+	"D4": 62,
+	"D#4": 63,
+	"E4": 64,
+	"F4": 65,
+	"F#4": 66,
+	"G4": 67,
+	"G#4": 68,
+	"A4": 69,
+	"A#4": 70,
+	"B4": 71,
+	"C5": 72,
+	"C#5": 73,
+	"D5": 74,
+	"D#5": 75,
+	"E5": 76,
+	"F5": 77,
+	"F#5": 78,
+	"G5": 79,
+	"G#5": 80,
+	"A5": 81,
+	"A#5": 82,
+	"B5": 83,
+	"C6": 84,
+	"C#6": 85,
+	"D6": 86,
+	"D#6": 87,
+	"E6": 88,
+	"F6": 89,
+	"F#6": 90,
+	"G6": 91,
+	"G#6": 92,
+	"A6": 93,
+	"A#6": 94,
+	"B6": 95,
+	"C7": 96,
+	"C#7": 97,
+	"D7": 98,
+	"D#7": 99,
+	"E7": 100,
+	"F7": 101,
+	"F#7": 102,
+	"G7": 103,
+	"G#7": 104,
+	"A7": 105,
+	"A#7": 106,
+	"B7": 107,
+	"C8": 108,
+	"C#8": 109,
+	"D8": 110,
+	"D#8": 111,
+	"E8": 112,
+	"F8": 113,
+	"F#8": 114,
+	"G8": 115,
+	"G#8": 116,
+	"A8": 117,
+	"A#8": 118,
+	"B8": 119,
+	"C9": 120,
+	"C#9": 121,
+	"D9": 122,
+	"D#9": 123,
+	"E9": 124,
+	"F9": 125,
+	"F#9": 126,
+	"G9": 127
+}
+
+const CONFIG_FILE_PATH = "user://PotatoMidi.json"
+var global_config_file_path = ProjectSettings.globalize_path(CONFIG_FILE_PATH)
+
 # MIDI configuration
 const MIDI_MIN_VELOCITY = 0.1
 const MIDI_MAX_VELOCITY = 1.0
@@ -11,9 +407,8 @@ onready var Chat = get_node("/root/ToesSocks/Chat")
 export(bool) var enable_bark_effects = true
 
 # Internal state
-var _state = {
-	"is_initialized": false
-}
+var _state = {"is_initialized": false}
+var config := {}
 
 
 class Instrument:
@@ -23,6 +418,7 @@ class Instrument:
 	var pitch: int
 
 
+# https://soundprogramming.net/file-formats/general-midi-instrument-list/
 var instruments = []
 
 onready var _guitar_strummer = preload("res://mods/PotatoMidi/Scripts/Guitar/GuitarStrummer.gd").new()
@@ -30,6 +426,9 @@ onready var _sfx_effect = preload("res://mods/PotatoMidi/Scripts/SFX/SFXEffect.g
 onready var _talk_effect = preload("res://mods/PotatoMidi/Scripts/Talk/TalkEffect.gd").new()
 onready var _default_config = preload("res://mods/PotatoMidi/default_config.gd").new()
 onready var JSONMinifier = preload("res://mods/PotatoMidi/Lib/json_minifier.gd").new()
+
+var DEBUG = false
+
 
 func _setup_midi():
 	if OS.open_midi_inputs():
@@ -39,49 +438,60 @@ func _setup_midi():
 		Chat.notify("Midi: Failed to open MIDI inputs")
 
 
-func _input(event):
+func _input(event: InputEvent) -> void:
 	var player = Players.local_player
-	if not event is InputEventMIDI: return
+	if not event is InputEventMIDI:
+		return
+	if not event.message == MIDI_MESSAGE_NOTE_ON:
+		return
+	if DEBUG:
+		print(
+			{
+				"channel": event.channel,
+				"instrument": event.instrument,
+				"pitch": event.pitch,
+				"pressure": event.pressure,
+				"velocity": event.velocity
+			}
+		)
 
 	if event.message == MIDI_MESSAGE_NOTE_ON and event.velocity > 0:
 		_handle_note_on(event)
+
 
 var _last_config: String = ""
 
 var _last_modified_time: int = 0
 
-func _load_config():
-	var config_file = File.new()
-	if config_file.file_exists("user://PotatoMidi.json"):
-		var current_modification_time = config_file.get_modified_time("user://PotatoMidi.json")
-		if current_modification_time == _last_modified_time:
-			return null
 
-		_last_modified_time = current_modification_time
-		config_file.open("user://PotatoMidi.json", File.READ)
-		var user_config_content = config_file.get_as_text()
-		if user_config_content == _last_config:
-			return null
-
-		Chat.notify("PotatoMidi: Config file modified, reloading...")
-		_last_config = user_config_content
-		config_file.close()
-
-		var parsed_json: JSONParseResult = JSON.parse(JSONMinifier.minify_json(user_config_content))
-		if parsed_json.error == OK:
-
-			return parsed_json.result
-		else:
-			Chat.notify("PotatoMidi: Failed to parse user config.json")
-			Chat.notify(parsed_json.error_string)
+func _load_config() -> Dictionary:
+	var new_config := {}
+	var config_file := File.new()
+	if config_file.file_exists(CONFIG_FILE_PATH):
+		# TODO: This is weird dont do this
+		var current_modification_time := config_file.get_modified_time(CONFIG_FILE_PATH)
+		if current_modification_time != _last_modified_time:
+			_last_modified_time = current_modification_time
+			config_file.open(CONFIG_FILE_PATH, File.READ)
+			var parsed_json := JSON.parse(JSONMinifier.minify_json(config_file.get_as_text()))
+			config_file.close()
+			if parsed_json.result.hash() != config.hash():
+				if parsed_json.error == OK:
+					if config.empty() == false:
+						Chat.notify("PotatoMidi: Config file modified, reloading...")
+					new_config = parsed_json.result
+				else:
+					Chat.write(
+						"PotatoMidi: Oops! Something is broken in your [url=file://%s]configuration JSON[/url]!" % global_config_file_path
+					)
+					Chat.write("[color=red]%s[/color]" % parsed_json.error_string)
 	else:
 		var default_config = _default_config.get_config()
-		config_file.open("user://PotatoMidi.json", File.WRITE)
+		config_file.open(CONFIG_FILE_PATH, File.WRITE)
 		config_file.store_string(JSON.print(default_config, "\t"))
 		config_file.close()
-		return default_config
+	return new_config
 
-	return null
 
 func _create_pitch_array(instrument: Dictionary):
 	if instrument.has("pitch_range"):
@@ -129,16 +539,16 @@ func _validate_instrument(instrument: Dictionary):
 	return true
 
 
-func _load_user_config():
+func _load_user_config() -> void:
 	var instruments_lookup = {
 		"guitar_strummer": funcref(_guitar_strummer, "input"),
 		"talk_effect": funcref(_talk_effect, "trigger_talk"),
 		"sfx": funcref(_sfx_effect, "trigger_sfx")
 	}
 
-	var config = _load_config()
+	config = _load_config()
 
-	if not config:
+	if config.empty():
 		return
 	instruments = []
 
@@ -164,7 +574,7 @@ func _load_user_config():
 			print("PotatoMidi: Unknown instrument: ", instrument_name)
 
 
-func _ready():
+func _ready() -> void:
 	print("PotatoMidi: Initializing MIDI system...")
 	var timer = Timer.new()
 	timer.wait_time = 2.0
